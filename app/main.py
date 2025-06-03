f0cb816f749c4ddd383bf82887de1d6aa3a113d4
afloresch <andres.flores.chong@gmail.com>
2025-06-03 12:28:35 -0400

    crea main.py para sección 2
    crea main.py para sección 2

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from app.consultas import router as consultas_router
+
+app = FastAPI(
+    title="API de Métricas SQL",
+    description="Endpoints para consultar contrataciones 2021",
+    version="2.0"
+)
+
+app.include_router(consultas_router)
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API de métricas activada"}
+
+@app.get("/salud")
+def salud():
+    return {"estado": "ok"}
